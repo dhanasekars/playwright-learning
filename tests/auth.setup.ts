dba132d08e5ef6c869479839f8228ce87e499afb
Dhanasekar <dhanasekars@gmail.com>
2024-03-19 09:23:52 +0530

    enviroment setup

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -6,29 +6,17 @@ import fs from 'fs'
 const authFile = '.auth/user.json'
 
 setup('authentication', async({page,request})=>{
-
-    // await page.goto('https://conduit.bondaracademy.com/')
-    // await page.waitForTimeout(500) 
-    // await page.getByText('Sign in').click()
-    // await page.getByRole('textbox', {name:"Email"}).fill('test@kani.com')
-    // await page.getByRole('textbox', {name:"Password"}).fill('welcome1')
-    // await page.waitForTimeout(500)
-    // await page.getByRole('button').click()
-    // await page.waitForResponse('https://conduit-api.bondaracademy.com/api/tags')
-    
-    // await page.context().storageState({path: authFile})
-
     const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login',{
         data:{
-            "user":{"email": "test@kani.com", "password": "welcome1"}
+            "user":{"email": process.env.USERNAME, "password":process.env.PASSWORD}
         }
     })
     expect(response.status()).toEqual(200)   
     const responseBody = await response.json()
     const accessToken = responseBody.user.token 
     user.origins[0].localStorage[0].value = accessToken
-    console.log(accessToken)
-    fs.writeFileSync(authFile, JSON.stringify(user))
+    // console.log(accessToken)
+    // fs.writeFileSync(authFile, JSON.stringify(user))
 
     process.env['ACCESS_TOKEN'] = accessToken
 })
